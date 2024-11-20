379e661f565af8dd5058ab6d5c25666c6c995caf
Carlos Casals <carlos.casals@docmorris.com>
2024-11-20 14:24:36 +0100

    new project

---
+++ src/app.py
@@ -0,0 +1,33 @@
+
+
+import os
+import warnings
+from dotenv import load_dotenv
+from layout import create_layout
+# from . import callbacks
+import dash_mantine_components as dmc
+from dash import Dash, _dash_renderer
+_dash_renderer._set_react_version("18.2.0")
+
+
+
+warnings.filterwarnings("ignore")
+load_dotenv()
+
+app = Dash(
+        __name__,
+        title="Alpen Consulting",
+        suppress_callback_exceptions=True,
+        # assets_folder="../assets/",
+        # external_stylesheets=["/assets/css/base_layout.css", "/assets/css/default_cards.css"],
+        external_stylesheets=dmc.styles.ALL
+    )
+
+server = app.server
+# No content is displayed before user validation
+# This could be replaced by cloud IAM/routing
+app.layout = create_layout()
+
+# # ------------------------------------------------------------------------------
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
---
+++ src/layout.py
@@ -0,0 +1,33 @@
+from dash import html, dcc
+import dash_mantine_components as dmc
+from datetime import datetime, timedelta, date
+from dash_iconify import DashIconify
+
+
+contruction_icon = DashIconify(icon="lucide:construction", style={"marginRight": 5})
+
+
+def create_layout() -> dmc.MantineProvider:
+    return dmc.MantineProvider(
+        [
+            dmc.Container(
+                size='xl',
+                style={"height": "100vh"},  # Set full height of the viewport
+                children=[
+                    dmc.Center(style={"height": "100%", "width": "100%"},
+                        children=[
+                            dmc.Stack([
+                                dmc.Image(
+                                    src="assets/icon/alpen_consulting_white.png",
+                                    style={"width": "60%"}
+                                ),
+                                dmc.Text([contruction_icon,"Under Construction"], c="grey")
+                            ],align="center",
+                            )
+                        ],
+                    ),
+                ]
+            )
+        ])
+
+   
