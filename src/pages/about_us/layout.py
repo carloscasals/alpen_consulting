711ee5b8980873b49a9c6129b22a42dc0ba5c0e6
Carlos Casals <carlos.casals@docmorris.com>
2024-11-23 16:36:01 +0100

    fix the responsive of the grid

--- src/pages/about_us/layout.py
+++ src/pages/about_us/layout.py
@@ -17,57 +17,42 @@ def create_layout() -> dmc.MantineProvider:
                 size='xl',
                 style={"height": "100vh"},  # Set full height of the viewport
                 children=[
-                    dmc.Grid(
-                        gutter=12,
+                    dmc.SimpleGrid(
+                        cols={"base": 1, "sm": 2, "lg": 3},
+                        spacing="sm",
                         children=[
-                            dmc.GridCol(html.Div([
-                                    html.H1("About us",style={"marginBottom":5}),
-                                    html.H3("A team of passionate consultants and industry experts",style={"marginTop":5})
-                                ])),
-                            dmc.GridCol(
-                                span="auto",
+                            dmc.Grid(
+                                gutter=8,
                                 children=[
-                                    # 1st consultant
-                                    dmc.Grid(
-                                        gutter=8,
-                                        children=[
-                                            dmc.GridCol(span=4,children=[
-                                                    dmc.Image(src="https://media.licdn.com/dms/image/v2/C5603AQHESZBGoc7QNQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1584210187270?e=1737590400&v=beta&t=uxLPxg0DHctF9hK2TQ24l6foWq1Qh154I3DSgzT9MIM",
-                                                    h=160,
-                                                    alt="Norway",
-                                                ),    
-                                            ]),
-                                            dmc.GridCol(span=8,children=[
-                                                    dmc.Text(["César Ferrer", html.A(linkedin_icon,href="https://www.linkedin.com/in/c%C3%A9sar-ferrer/")], fw=500, size="lg", c="blue"),
-                                                    dmc.Text("Managing Partner", fw=700),
-                                                    dmc.Text("Strategy and management consultant with almost 20 years in operations helping PE, investement funds and family-owned clients at McKinsey & Accenture", size="sm",),  
-                                            ])
+                                    dmc.GridCol(span=4,children=[
+                                            dmc.Image(src="https://media.licdn.com/dms/image/v2/C5603AQHESZBGoc7QNQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1584210187270?e=1737590400&v=beta&t=uxLPxg0DHctF9hK2TQ24l6foWq1Qh154I3DSgzT9MIM",
+                                            h=160,
+                                            alt="Norway",
+                                        ),    
                                     ]),
-                                ]
-                            ),
-                            dmc.GridCol(
-                                span="auto",
+                                    dmc.GridCol(span=8,children=[
+                                            dmc.Text(["César Ferrer", html.A(linkedin_icon,href="https://www.linkedin.com/in/c%C3%A9sar-ferrer/")], fw=500, size="lg", c="blue"),
+                                            dmc.Text("Managing Partner", fw=700),
+                                            dmc.Text("Strategy and management consultant with almost 20 years in operations helping PE, investement funds and family-owned clients at McKinsey & Accenture", size="sm",),  
+                                    ])
+                            ]),
+                            dmc.Grid(
+                                gutter=8,
                                 children=[
-                                    # 2nd consultant
-                                    dmc.Grid(
-                                        gutter=8,
-                                        children=[
-                                            dmc.GridCol(span=4,children=[
-                                                    dmc.Image(src="https://media.licdn.com/dms/image/v2/C4E03AQE6dU65Ksm0sg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1522928486611?e=1737590400&v=beta&t=D1UgkwtMumXLaUBz4AEsZW4_a28jGg_ztyIzDDHDr54",
-                                                    h=160,
-                                                    alt="Norway",
-                                                ),    
-                                            ]),
-                                            dmc.GridCol(span=8,children=[
-                                                    dmc.Text(["Marta Serradell", html.A(linkedin_icon,href="https://www.linkedin.com/in/marta-serradell-bustamante/")], fw=500, size="lg", c="blue"),
-                                                    dmc.Text("Senior Advisor - Finance", fw=700),
-                                                    dmc.Text("Finance executive and consultant with +10 years of experience in the educational sector (schools, universities and language)", size="sm",),  
-                                            ])
+                                    dmc.GridCol(span=4,children=[
+                                            dmc.Image(src="https://media.licdn.com/dms/image/v2/C4E03AQE6dU65Ksm0sg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1522928486611?e=1737590400&v=beta&t=D1UgkwtMumXLaUBz4AEsZW4_a28jGg_ztyIzDDHDr54",
+                                            h=160,
+                                            alt="Norway",
+                                        ),    
                                     ]),
-                                ]
-                            )
+                                    dmc.GridCol(span=8,children=[
+                                            dmc.Text(["Marta Serradell", html.A(linkedin_icon,href="https://www.linkedin.com/in/marta-serradell-bustamante/")], fw=500, size="lg", c="blue"),
+                                            dmc.Text("Senior Advisor - Finance", fw=700),
+                                            dmc.Text("Finance executive and consultant with +10 years of experience in the educational sector (schools, universities and language)", size="sm",),  
+                                    ])
+                            ]),
                         ]
-                    )
+                    ),
                 ]
             )
         ])
