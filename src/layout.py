1f59e357363f21fd251fac47fb4f9b559bd391e1
Carlos Casals <carlos.casals@docmorris.com>
2024-11-26 15:55:31 +0100

    for now share team and about_us

--- src/layout.py
+++ src/layout.py
@@ -25,7 +25,7 @@ def create_layout():
                                     style={"width": "130px"}
                                 ), 
                                 href="/"),
-                                dmc.Anchor("About us", href="/about_us", className="anchor-link"),
+                                dmc.Anchor("About us", href="/team", className="anchor-link"),
                                 dmc.Anchor("Value Proposition", href="/value_proposition", className="anchor-link"),
                                 dmc.Anchor("Approach", href="/approach", className="anchor-link"),
                                 dmc.Anchor("Team", href="/team", className="anchor-link"),
